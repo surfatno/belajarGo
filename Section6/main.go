523ebfb613a77302f27bd89cceac61657f2d6585
surfatno <surfatnow@gmail.com>
2022-08-18 15:10:44 +0700

    Section 7 Done

---
+++ Section6/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+)
+
+//go:embed version.txt
+var version string
+
+//go:embed logo.jpg
+var logo []byte
+
+//go:embed files/*.txt
+var path embed.FS
+
+// Step 1 : go build
+// Step 2 : ./Section6.exe
+func main() {
+	fmt.Println(version)
+
+	err := ioutil.WriteFile("logo_new.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	dir, _ := path.ReadDir("files")
+	for _, entry := range dir {
+		if !entry.IsDir() {
+			fmt.Println(entry.Name())
+			file, _ := path.ReadFile("files/" + entry.Name())
+			fmt.Println(string(file))
+		}
+	}
+}
